60d77b15d13b453786c147a6a2354e1d6cef03ea
Cgarcia123 <jgarcia@mowa.com.pe>
2017-10-14 10:43:52 -0500

    Cambios sabado 14 de octubre

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -3,12 +3,12 @@
 @section('main')
 <div class="row-fluid">
     <div class="row" style="margin-left: 20px;">
-        <div class="span2"><span class="label label-important" style="background-color:  #00cfff!important">&nbsp;&nbsp;&nbsp;</span> Por cargar</div>
-        <div class="span2"><span class="label label-important label-block" style="background-color: #ef0000!important">&nbsp;&nbsp;&nbsp;</span> Cargando archivo</div>
-        <div class="span2"><span class="label label-important" style="background-color:  #ff7300!important">&nbsp;&nbsp;&nbsp;</span> Pendiente de envio</div>
-        <div class="span2"><span class="label label-important" style="background-color:  #f7007c!important">&nbsp;&nbsp;&nbsp;</span> Detenida</div>
+        <div class="span2"><span class="label label-important" style="background-color:  #000000!important">&nbsp;&nbsp;&nbsp;</span> Por cargar</div>
+        <div class="span2"><span class="label label-important label-block" style="background-color: #ffa300!important">&nbsp;&nbsp;&nbsp;</span> Cargando archivo</div>
+        <div class="span2"><span class="label label-important" style="background-color:  #fffa48!important">&nbsp;&nbsp;&nbsp;</span> Pendiente de envio</div>
+        <div class="span2"><span class="label label-important" style="background-color:  #ff000d!important">&nbsp;&nbsp;&nbsp;</span> Detenida</div>
         <div class="span2"><span class="label label-important" style="background-color:  #08a531!important">&nbsp;&nbsp;&nbsp;</span> Enviando </div>
-        <div class="span2"><span class="label label-important" style="background-color:  #0d3cbf!important">&nbsp;&nbsp;&nbsp;</span> Finalizada</div>
+        <div class="span2"><span class="label label-important" style="background-color:  #296aa6!important">&nbsp;&nbsp;&nbsp;</span> Finalizada</div>
     </div><br>
     <div id="gantt_here" style='width:100%; min-height: 450px;; max-height: 500px;'></div>
 </div>
